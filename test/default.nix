643218711aeb5e2e7a0d79724df3284a3f6e845f
justinwoo <moomoowoo@gmail.com>
2018-11-04 02:48:42 +0200

    just use parent/curr dir from test

--- test/default.nix
+++ test/default.nix
@@ -8,12 +8,17 @@ let
     sha256 = "0njhcl7dq58b3kmjbz6ndsccv4pcmdxc5lg7p13115phcmznpn99";
   });
 
-  psc-package2nix = import (pkgs.fetchFromGitHub {
+  # this is how you would normally import psc-package2nix to your project
+  _psc-package2nix = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "psc-package2nix";
+    # you should make sure to use nix-prefetch-github to get the latest rev and sha!
     rev = "a99092bfe9c32e0df9a826ccc509bd02b7b192c1";
     sha256 = "0296jb8b47zxnnhrjp4ky1mj835jff59sx4przix652mclsy13r3";
   }) {};
+
+  # we override the remote with the local definition here
+  psc-package2nix = import ../. {};
 
 in pkgs.stdenv.mkDerivation {
   name = "test";
