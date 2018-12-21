5d0f14362fecdba536272845f9a63c18bf27c9bd
justinwoo <moomoowoo@gmail.com>
2018-12-21 17:44:53 +0800

    compile with packages information

--- test/default.nix
+++ test/default.nix
@@ -2,14 +2,12 @@
 
 let
   easy-ps = import ./easy-ps.nix { inherit pkgs; };
-
   psc-package2nix = import ./psc-package2nix.nix { inherit pkgs; };
 
 in pkgs.stdenv.mkDerivation {
   name = "test";
   buildInputs = [
     easy-ps.inputs.purs
-    easy-ps.inputs.psc-package-simple
     psc-package2nix
   ];
 }
